aba8513a3dda8e1b7fc8616861fe79a542a8f626
Duvan Candelario <duvan.programador@gmail.com>
2023-09-26 18:30:14 -0500

    Culminamos el parcial

---
+++ main.js
@@ -0,0 +1,20 @@
+function mostrarOpciones(valueFormulario) {
+    
+    var seccionRegistro = document.getElementById("seccionRegistro");
+    var nombre = document.getElementById("nombre");
+    var apellido = document.getElementById("apellido");
+    var numero = document.getElementById("numero");
+    var contrasena = document.getElementById("contrasena");
+    var correo = document.getElementById("correo");
+
+    if (valueFormulario === "logiin") {
+
+        seccionRegistro.classList.add("seccionRegistro");
+        
+    } else if (valueFormulario === "registro") {
+
+        seccionRegistro.classList.remove("seccionRegistro");
+        
+        
+    }
+}
